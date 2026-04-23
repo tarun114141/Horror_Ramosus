b81c042b9c3102e53b5acdcce91a410c99f16a38
tarun114141 <202401040033@mitaoe.ac.in>
2026-04-23 18:31:27 +0530

    y_sorting on the outside scene

--- test_player/player.gd
+++ test_player/player.gd
@@ -1,37 +1,24 @@
 extends CharacterBody2D
 
-@export var speed := 100
-
-@onready var anim = $AnimatedSprite2D
-
+@export var speed: float = 200.0
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta):
-
 	var input_vector = Vector2.ZERO
-	
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 
-	input_vector = input_vector.normalized()
+	if input_vector != Vector2.ZERO:
+		input_vector = input_vector.normalized()
 
-	# Convert to isometric movement
-	var iso_vector = Vector2(
-		input_vector.x - input_vector.y,
-		(input_vector.x + input_vector.y) / 2
-	)
-
-	velocity = iso_vector * speed
+	velocity = input_vector * speed
 	move_and_slide()
-
 	play_animation(input_vector)
 
-
-func play_animation(direction):
-
+func play_animation(direction: Vector2):
 	if direction == Vector2.ZERO:
 		anim.stop()
 		return
-
 	if abs(direction.x) > abs(direction.y):
 		if direction.x > 0:
 			anim.play("right")
