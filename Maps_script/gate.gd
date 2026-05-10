d4bf4ac29c45e5e07c1a190dbd026694f6360489
tarun114141 <202401040033@mitaoe.ac.in>
2026-05-10 23:34:56 +0530

    Outside expansion , collision addition and gate animation

---
+++ Maps_script/gate.gd
@@ -0,0 +1,32 @@
+extends StaticBody2D
+
+
+@onready var gate: AnimatedSprite2D = $AnimatedSprite2D
+
+var player_inside= false
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("open") and player_inside:
+		gate.play()
+	pass
+
+
+func _on_gate_open_body_entered(body: Node2D) -> void:
+	player_inside=true
+	
+		
+
+
+
+func _on_gate_open_body_exited(body: Node2D) -> void:
+	player_inside=false
+	
