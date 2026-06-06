ad08779964e2f0adf7fecfa8406d7f054273c73f
tarun114141 <202401040033@mitaoe.ac.in>
2026-06-06 23:34:03 +0530

    Checking something and added a portal with from gate to reception. Long time no see

--- Maps_script/gate.gd
+++ Maps_script/gate.gd
@@ -4,6 +4,8 @@ extends StaticBody2D
 @onready var gate: AnimatedSprite2D = $AnimatedSprite2D
 
 var player_inside= false
+@export var next_scene : PackedScene
+
 
 
 
@@ -17,6 +19,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("open") and player_inside:
 		gate.play()
+		get_tree().change_scene_to_packed(next_scene)
 	pass
 
 
